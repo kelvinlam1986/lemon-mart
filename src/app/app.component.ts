8357a35eba677718e28bfa4925be4bccd3f2aa67
kelvinlam <kelvincoder@gmail.com>
2019-02-28 10:53:43 +0700

    Implement design basic UI for toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   template: `
     <mat-toolbar color="primary">
+      <button mat-icon-button><mat-icon>menu</mat-icon></button>
+      <mat-icon svgIcon="lemon"></mat-icon>
       <a mat-button router-link="/home"><h1>Lemon Mart</h1></a>
+      <span class="flex-spacer"></span>
+      <button	mat-icon-button><mat-icon>account_circle</mat-icon></button>
+      <button	mat-icon-button><mat-icon>lock_open</mat-icon></button>
     </mat-toolbar>
     <router-outlet></router-outlet>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'lemon',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/lemon.svg')
+    );
+  }
   title = 'lemon-mart';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,9 +21,11 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FlexLayoutModule,
     ManagerModule,
     InventoryModule,
     PosModule,
