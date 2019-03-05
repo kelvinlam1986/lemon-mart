f1fa0b2c1bdb0f638ec126896270793bd546fa96
kelvinlam <kelvincoder@gmail.com>
2019-03-05 18:53:00 +0700

    Add Manager routing

--- src/app/manager/manager-routing.module.ts
+++ src/app/manager/manager-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ManagerHomeComponent } from '../manager/manager-home/manager-home.component';
+import { ManagerComponent } from './manager.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ManagerComponent, children: [
+      { path: '', redirectTo: '/manager/home', pathMatch: 'full' },
+      { path: 'home', component: ManagerHomeComponent },
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MaterialModule } from '../material.module';
 import { ManagerRoutingModule } from './manager-routing.module';
+import { ManagerHomeComponent } from './manager-home/manager-home.component';
+import { ManagerComponent } from './manager.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ManagerHomeComponent, ManagerComponent],
   imports: [
     CommonModule,
-    ManagerRoutingModule
+    ManagerRoutingModule,
+    MaterialModule
   ]
 })
 export class ManagerModule { }
